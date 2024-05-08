2351beeb5620061b784eb0d9764df448a36733c6
Abdulganiu Agboola <agboolaganiu247@gmail.com>
2024-05-08 17:39:08 -0400

    adding a jenkinsfile

--- abdul.tf
+++ abdul.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "abdul" {
   ami = "ami-04ff98ccbfa41c9ad"
   instance_type = "t2.micro"
   key_name = "okkk"
+  tags = {
+    Name = "Abdul-server"
+    Environment = "DevOps"
+  }
 }
